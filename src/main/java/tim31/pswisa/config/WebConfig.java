cf062bca2250912b9dae567b60a3fbddef0044cc
m1l1c4 <57289672+m1l1c4@users.noreply.github.com>
2020-02-13 19:21:31 +0100

    Update WebConfig.java

--- src/main/java/tim31/pswisa/config/WebConfig.java
+++ src/main/java/tim31/pswisa/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("https://test-ivana-dms.herokuapp.com/");
+		registry.addMapping("/**").allowedOrigins("*");
 	}
 }
